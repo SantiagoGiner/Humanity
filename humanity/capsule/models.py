6d602f54de2a4190abf334bb6d36bc034b2d423f
Santiago Giner <santiagoginer@college.harvard.edu>
2020-11-22 15:06:25 -0400

    Add first model

--- humanity/capsule/models.py
+++ humanity/capsule/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    name = models.TextField()
+    def __str__(self):
+        return f"{self.name}"
+
+class JournalEntry(models.Model):
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="journal_entry")
+    entry = models.TextField()
+    date = models.DateField()
+    def __str__(self):
+        return f"Journal entry #{self.id}: {self.date}"
+
+class Goal(models.Model):
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="goal")
+    goal = models.TextField()
+    priority = models.CharField(max_length=9, default='daily')
+    def __str__(self):
+        if self.priority == 'long-term':
+            return f"Long-term goal to: {self.goal}"
+        elif self.priority == 'monthly':
+            return f"Monthly goal to: {self.goal}"
+        elif self.priority == 'weekly':
+            return f"Weekly goal to: {self.goal}"
+        else:
+            return f"Daily goal to: {self.goal}"
