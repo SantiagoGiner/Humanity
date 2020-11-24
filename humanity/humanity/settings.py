00d7d9da06914e0515afc4042ac154ce2f346138
Santiago Giner <santiagoginer@college.harvard.edu>
2020-11-24 14:30:10 -0400

    Fix login feature

--- humanity/humanity/settings.py
+++ humanity/humanity/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!pxs*_yyqs%h$r7_2$k4^u)3g!8#7usfk0%c1b!*%(vl#+t#0d'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
