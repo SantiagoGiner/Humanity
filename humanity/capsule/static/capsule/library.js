cbe79004a188047839fe06e49580899bccfbca2a
Santiago Giner <santiagoginer@college.harvard.edu>
2020-12-04 21:44:09 -0400

    Added error checking in AJAX call

--- humanity/capsule/static/capsule/library.js
+++ humanity/capsule/static/capsule/library.js
@@ -11,34 +11,47 @@ document.addEventListener('DOMContentLoaded', () => {
             dataType: 'json',
             // If the query is successful
             success: (data) => {
-                let html = '';
-                // Display the query results, including book title, author(s), and cover photo
-                for (let i = 0; i < data.items.length; i++) {
-                    // Form that when submitted will indicate that the user has selected one book
-                    html += `<form method="post">`;
-                    // Display the book title, with a hidden input containing said title
-                    html += `<p class="container my-2">Title: ${data.items[i].volumeInfo.title}</p>`;
-                    html += `<input name="title" style="display:none;" 
-                              value="${data.items[i].volumeInfo.title}">`;
-                    // Display the author(s) along with another hidden input containing these
-                    html += `<p class="container my-2">Author(s): ${data.items[i].volumeInfo.authors}</p>`;
-                    html += `<input name="author" style="display:none;" 
-                              value="${data.items[i].volumeInfo.authors}">`;
-                    // If there are image links, display the cover photo
-                    if (data.items[i].volumeInfo.imageLinks) {
-                        html += `<img class="img mb-4" 
-                                  src="${data.items[i].volumeInfo.imageLinks.thumbnail}">`;
-                        // Hidden input with the image source
-                        html += `<input name="cover" style="display:none;" 
-                                  value="${data.items[i].volumeInfo.imageLinks.thumbnail}">`;
+                // If query returned a result
+                if (data.items != undefined) {
+                    let html = '';
+                    // Display the query results, including book title, author(s), and cover photo
+                    for (let i = 0; i < data.items.length; i++) {
+                        // Form that when submitted will indicate that the user has selected one book
+                        html += `<form method="post">`;
+                        // Display the book title, with a hidden input containing said title
+                        html += `<p class="container my-2">Title: 
+                                  ${data.items[i].volumeInfo.title}</p>`;
+                        html += `<input name="title" style="display:none;" 
+                                value="${data.items[i].volumeInfo.title}">`;
+                        // Display the author(s) along with another hidden input containing these
+                        html += `<p class="container my-2">Author(s): 
+                                 ${data.items[i].volumeInfo.authors}</p>`;
+                        html += `<input name="author" style="display:none;" 
+                                value="${data.items[i].volumeInfo.authors}">`;
+                        // If there are image links, display the cover photo
+                        if (data.items[i].volumeInfo.imageLinks) {
+                            html += `<img class="img mb-4" 
+                                    src="${data.items[i].volumeInfo.imageLinks.thumbnail}">`;
+                            // Hidden input with the image source
+                            html += `<input name="cover" style="display:none;" 
+                                    value="${data.items[i].volumeInfo.imageLinks.thumbnail}">`;
+                        }
+                        // Add the select button and end the form
+                        html += `<button class="btn btn-success mx-2" type="submit">Select</button>`;
+                        html += '</form>'
+                        html += '<hr>';
                     }
-                    // Add the select button and end the form
-                    html += `<button class="btn btn-success mx-2" type="submit">Select</button>`;
-                    html += '</form>'
-                    html += '<hr>';
+                    // Set the HTML of the search result section to be the newly generated
+                    searchResult.innerHTML = html;
                 }
-                // Set the HTML of the search result section to be the newly generated
-                searchResult.innerHTML = html;
+                // If query did not return any results, inform the user so
+                else {
+                    searchResult.innerHTML = '<h4>That query did not return any results!</h4>';
+                }
+            },
+            // If query could not be carried out, inform the user thusly
+            error: () => {
+                searchResult.innerHTML = '<h4>Invalid input: please try with different words</h4>';
             }
         });
     }
